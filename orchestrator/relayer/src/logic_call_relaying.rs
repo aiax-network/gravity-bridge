9a9ddda71d3d740240efa1b457797d61cf0c14f8
Collin Brittain <collinbrittain@gmail.com>
2021-11-10 14:46:55 -0600

    More of the same kind of changes to logic_call_relaying.rs

--- orchestrator/relayer/src/logic_call_relaying.rs
+++ orchestrator/relayer/src/logic_call_relaying.rs
@@ -26,16 +26,24 @@ pub async fn relay_logic_calls(
     timeout: Duration,
     gas_multiplier: f32,
 ) {
+    // Leaving this naked unwrap() as is because main_loop must have successfully derived
+    // the public key already in order to get this far.
     let our_ethereum_address = ethereum_key.to_public_key().unwrap();
 
-    let latest_calls = get_latest_logic_calls(grpc_client).await;
-    trace!("Latest Logic calls {:?}", latest_calls);
-    if latest_calls.is_err() {
-        return;
-    }
-    let latest_calls = latest_calls.unwrap();
+    let latest_calls = match get_latest_logic_calls(grpc_client).await {
+        Ok(calls) =>  {
+            trace!("Latest Logic calls {:?}", calls);
+            calls
+        },
+        Err(err) => {
+            error!("Error while retrieving latest logic calls: {:?}", err);
+            return;
+        },
+    };
+
     let mut oldest_signed_call: Option<LogicCall> = None;
     let mut oldest_signatures: Option<Vec<LogicCallConfirmResponse>> = None;
+
     for call in latest_calls {
         let sigs = get_logic_call_signatures(
             grpc_client,
@@ -43,9 +51,12 @@ pub async fn relay_logic_calls(
             call.invalidation_nonce,
         )
         .await;
+
         trace!("Got sigs {:?}", sigs);
+
         if let Ok(sigs) = sigs {
             let hash = encode_logic_call_confirm_hashed(gravity_id.clone(), call.clone());
+
             // this checks that the signatures for the batch are actually possible to submit to the chain
             if current_valset.order_sigs(&hash, &sigs).is_ok() {
                 oldest_signed_call = Some(call);
@@ -66,31 +77,35 @@ pub async fn relay_logic_calls(
             );
         }
     }
+
+    // Does this need a higher level log message? Information?
     if oldest_signed_call.is_none() {
         trace!("Could not find Call with signatures! exiting");
         return;
     }
+
     let oldest_signed_call = oldest_signed_call.unwrap();
     let oldest_signatures = oldest_signatures.unwrap();
-
-    let latest_ethereum_call = get_logic_call_nonce(
+    let latest_ethereum_call = match get_logic_call_nonce(
         gravity_contract_address,
         oldest_signed_call.invalidation_id.clone(),
         our_ethereum_address,
         web3,
     )
-    .await;
-    if latest_ethereum_call.is_err() {
-        error!(
-            "Failed to get latest Ethereum LogicCall with {:?}",
-            latest_ethereum_call
-        );
-        return;
-    }
-    let latest_ethereum_call = latest_ethereum_call.unwrap();
+    .await {
+        Ok(nonce) => nonce,
+        Err(err) => {
+            error!(
+                "Failed to get latest Ethereum LogicCall with {:?}",
+                err
+            );
+            return;
+        }
+    };
     let latest_cosmos_call_nonce = oldest_signed_call.clone().invalidation_nonce;
+
     if latest_cosmos_call_nonce > latest_ethereum_call {
-        let cost = ethereum_gravity::logic_call::estimate_logic_call_cost(
+        let cost = match ethereum_gravity::logic_call::estimate_logic_call_cost(
             current_valset.clone(),
             oldest_signed_call.clone(),
             &oldest_signatures,
@@ -99,12 +114,14 @@ pub async fn relay_logic_calls(
             gravity_id.clone(),
             ethereum_key,
         )
-        .await;
-        if cost.is_err() {
-            error!("LogicCall cost estimate failed with {:?}", cost);
-            return;
-        }
-        let cost = cost.unwrap();
+        .await {
+            Ok(cost) => cost,
+            Err(err) => {
+                error!("LogicCall cost estimate failed with {:?}", err);
+                return;
+            }
+        };
+
         info!(
                 "We have detected latest LogicCall {} but latest on Ethereum is {} This LogicCall is estimated to cost {} Gas / {:.4} ETH to submit",
                 latest_cosmos_call_nonce,
@@ -113,8 +130,8 @@ pub async fn relay_logic_calls(
                 downcast_to_u128(cost.get_total()).unwrap() as f32
                     / downcast_to_u128(one_eth()).unwrap() as f32
             );
-        let tx_options = vec![SendTxOption::GasPriceMultiplier(gas_multiplier)];
 
+        let tx_options = vec![SendTxOption::GasPriceMultiplier(gas_multiplier)];
         let res = send_eth_logic_call(
             current_valset,
             oldest_signed_call,
@@ -127,6 +144,7 @@ pub async fn relay_logic_calls(
             tx_options,
         )
         .await;
+
         if res.is_err() {
             info!("LogicCall submission failed with {:?}", res);
         }
