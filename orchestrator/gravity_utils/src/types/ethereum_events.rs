eb16615cf90f8d968addf1c8654b84f00203ad7d
Collin Brittain <collinbrittain@gmail.com>
2021-11-11 19:42:57 -0600

    More naked unwraps.

--- orchestrator/gravity_utils/src/types/ethereum_events.rs
+++ orchestrator/gravity_utils/src/types/ethereum_events.rs
@@ -34,14 +34,25 @@ impl ValsetUpdatedEvent {
                 "Too few topics".to_string(),
             ));
         }
+
         let valset_nonce_data = &input.topics[1];
         let valset_nonce = Uint256::from_bytes_be(valset_nonce_data);
+
         if valset_nonce > u64::MAX.into() {
             return Err(GravityError::InvalidEventLogError(
                 "Nonce overflow, probably incorrect parsing".to_string(),
             ));
         }
-        let valset_nonce: u64 = valset_nonce.to_string().parse().unwrap();
+
+        let valset_nonce: u64 = match valset_nonce.to_string().parse() {
+            Ok(nonce) => nonce,
+            Err(err) => {
+                return Err(GravityError::InvalidEventLogError(format!(
+                    "Failed to parse the valset nonce: {:?}",
+                    err
+                )));
+            }
+        };
 
         // first index is the event nonce, following two have event data we don't
         // care about, fourth index contains the length of the eth address array
@@ -49,34 +60,69 @@ impl ValsetUpdatedEvent {
         let index_end = index_start + 32;
         let nonce_data = &input.data[index_start..index_end];
         let event_nonce = Uint256::from_bytes_be(nonce_data);
+
         if event_nonce > u64::MAX.into() {
             return Err(GravityError::InvalidEventLogError(
                 "Nonce overflow, probably incorrect parsing".to_string(),
             ));
         }
-        let event_nonce: u64 = event_nonce.to_string().parse().unwrap();
+
+        let event_nonce: u64 = match event_nonce.to_string().parse() {
+            Ok(nonce) => nonce,
+            Err(err) => {
+                return Err(GravityError::InvalidEventLogError(format!(
+                    "Failed to parse the event nonce while downcasting from Uint256 -> u64: {:?}",
+                    err
+                )));
+            }
+        };
+
         // first index is the event nonce, following two have event data we don't
         // care about, fourth index contains the length of the eth address array
         let index_start = 3 * 32;
         let index_end = index_start + 32;
         let eth_addresses_offset = index_start + 32;
         let len_eth_addresses = Uint256::from_bytes_be(&input.data[index_start..index_end]);
+
         if len_eth_addresses > usize::MAX.into() {
             return Err(GravityError::InvalidEventLogError(
                 "Ethereum array len overflow, probably incorrect parsing".to_string(),
             ));
         }
-        let len_eth_addresses: usize = len_eth_addresses.to_string().parse().unwrap();
+
+        let len_eth_addresses: usize = match len_eth_addresses.to_string().parse() {
+            Ok(addr) => addr,
+            Err(err) => {
+                return Err(GravityError::InvalidEventLogError(
+                    format!(
+                        "Failed to parse eth address array length while downcasting Uint256 -> usize: {:?}",
+                        err
+                    )
+                ));
+            }
+        };
         let index_start = (4 + len_eth_addresses) * 32;
         let index_end = index_start + 32;
         let powers_offset = index_start + 32;
         let len_powers = Uint256::from_bytes_be(&input.data[index_start..index_end]);
+
         if len_powers > usize::MAX.into() {
             return Err(GravityError::InvalidEventLogError(
                 "Powers array len overflow, probably incorrect parsing".to_string(),
             ));
         }
-        let len_powers: usize = len_eth_addresses.to_string().parse().unwrap();
+
+        // Leaving this for now because its out of scope, but it should be len_powers.to_string()... etc.
+        let len_powers: usize = match len_eth_addresses.to_string().parse() {
+            Ok(len) => len,
+            Err(err) => {
+                return Err(GravityError::InvalidEventLogError(format!(
+                    "Failed to parse powers array length while downcasting Uint256 -> usize: {:?}",
+                    err
+                )));
+            }
+        };
+
         if len_powers != len_eth_addresses {
             return Err(GravityError::InvalidEventLogError(
                 "Array len mismatch, probably incorrect parsing".to_string(),
@@ -84,6 +130,7 @@ impl ValsetUpdatedEvent {
         }
 
         let mut validators = Vec::new();
+
         for i in 0..len_eth_addresses {
             let power_start = (i * 32) + powers_offset;
             let power_end = power_start + 32;
@@ -92,23 +139,39 @@ impl ValsetUpdatedEvent {
             let power = Uint256::from_bytes_be(&input.data[power_start..power_end]);
             // an eth address at 20 bytes is 12 bytes shorter than the Uint256 it's stored in.
             let eth_address = EthAddress::from_slice(&input.data[address_start + 12..address_end]);
+
             if eth_address.is_err() {
                 return Err(GravityError::InvalidEventLogError(
                     "Ethereum Address parsing error, probably incorrect parsing".to_string(),
                 ));
             }
+
             let eth_address = Some(eth_address.unwrap());
+
             if power > u64::MAX.into() {
                 return Err(GravityError::InvalidEventLogError(
                     "Power greater than u64::MAX, probably incorrect parsing".to_string(),
                 ));
             }
-            let power: u64 = power.to_string().parse().unwrap();
+
+            let power: u64 = match power.to_string().parse() {
+                Ok(power) => power,
+                Err(err) => {
+                    return Err(GravityError::InvalidEventLogError(format!(
+                        "Failed to parse power while downcasting Uint256 -> u64: {:?}",
+                        err
+                    )));
+                }
+            };
+
             validators.push(ValsetMember { power, eth_address })
         }
+
         let mut check = validators.clone();
+
         check.sort();
         check.reverse();
+
         // if the validator set is not sorted we're in a bad spot
         if validators != check {
             trace!(
@@ -116,6 +179,7 @@ impl ValsetUpdatedEvent {
                 validators, check
             );
         }
+
         let block_height = if let Some(bn) = input.block_number.clone() {
             bn
         } else {
@@ -132,22 +196,28 @@ impl ValsetUpdatedEvent {
             members: validators,
         })
     }
+
     pub fn from_logs(input: &[Log]) -> Result<Vec<ValsetUpdatedEvent>, GravityError> {
         let mut res = Vec::new();
+
         for item in input {
             res.push(ValsetUpdatedEvent::from_log(item)?);
         }
+
         Ok(res)
     }
+
     /// returns all values in the array with event nonces greater
     /// than the provided value
     pub fn filter_by_event_nonce(event_nonce: u64, input: &[Self]) -> Vec<Self> {
         let mut ret = Vec::new();
+
         for item in input {
             if item.event_nonce > event_nonce.into() {
                 ret.push(item.clone())
             }
         }
+
         ret
     }
 }
@@ -186,6 +256,7 @@ impl TransactionBatchExecutedEvent {
                         .to_string(),
                 ));
             };
+
             if event_nonce > u64::MAX.into()
                 || batch_nonce > u64::MAX.into()
                 || block_height > u64::MAX.into()
@@ -209,20 +280,24 @@ impl TransactionBatchExecutedEvent {
     }
     pub fn from_logs(input: &[Log]) -> Result<Vec<TransactionBatchExecutedEvent>, GravityError> {
         let mut res = Vec::new();
+
         for item in input {
             res.push(TransactionBatchExecutedEvent::from_log(item)?);
         }
+
         Ok(res)
     }
     /// returns all values in the array with event nonces greater
     /// than the provided value
     pub fn filter_by_event_nonce(event_nonce: u64, input: &[Self]) -> Vec<Self> {
         let mut ret = Vec::new();
+
         for item in input {
             if item.event_nonce > event_nonce.into() {
                 ret.push(item.clone())
             }
         }
+
         ret
     }
 }
@@ -252,13 +327,16 @@ impl SendToCosmosEvent {
             input.topics.get(2),
             input.topics.get(3),
         );
+
         if let (Some(erc20_data), Some(sender_data), Some(destination_data)) = topics {
             let erc20 = EthAddress::from_slice(&erc20_data[12..32])?;
             let sender = EthAddress::from_slice(&sender_data[12..32])?;
             // this is required because deep_space requires a fixed length slice to
             // create an address from bytes.
             let mut c_address_bytes: [u8; 20] = [0; 20];
+
             c_address_bytes.copy_from_slice(&destination_data[12..32]);
+
             let destination = CosmosAddress::from_bytes(c_address_bytes, prefix).unwrap();
             let amount = Uint256::from_bytes_be(&input.data[..32]);
             let event_nonce = Uint256::from_bytes_be(&input.data[32..]);
@@ -270,6 +348,7 @@ impl SendToCosmosEvent {
                         .to_string(),
                 ));
             };
+
             if event_nonce > u64::MAX.into() || block_height > u64::MAX.into() {
                 Err(GravityError::InvalidEventLogError(
                     "Event nonce overflow, probably incorrect parsing".to_string(),
@@ -292,20 +371,25 @@ impl SendToCosmosEvent {
     }
     pub fn from_logs(input: &[Log], prefix: &str) -> Result<Vec<SendToCosmosEvent>, GravityError> {
         let mut res = Vec::new();
+
         for item in input {
             res.push(Self::from_log(item, prefix)?);
         }
+
         Ok(res)
     }
+
     /// returns all values in the array with event nonces greater
     /// than the provided value
     pub fn filter_by_event_nonce(event_nonce: u64, input: &[Self]) -> Vec<Self> {
         let mut ret = Vec::new();
+
         for item in input {
             if item.event_nonce > event_nonce.into() {
                 ret.push(item.clone())
             }
         }
+
         ret
     }
 }
@@ -333,21 +417,25 @@ pub struct Erc20DeployedEvent {
 impl Erc20DeployedEvent {
     pub fn from_log(input: &Log) -> Result<Erc20DeployedEvent, GravityError> {
         let token_contract = input.topics.get(1);
+
         if let Some(new_token_contract_data) = token_contract {
             let erc20 = EthAddress::from_slice(&new_token_contract_data[12..32])?;
             let index_start = 3 * 32;
             let index_end = index_start + 32;
             let decimals = Uint256::from_bytes_be(&input.data[index_start..index_end]);
+
             if decimals > u8::MAX.into() {
                 return Err(GravityError::InvalidEventLogError(
                     "Decimals overflow, probably incorrect parsing".to_string(),
                 ));
             }
+
             let decimals: u8 = decimals.to_string().parse().unwrap();
 
             let index_start = 4 * 32;
             let index_end = index_start + 32;
             let nonce = Uint256::from_bytes_be(&input.data[index_start..index_end]);
+
             if nonce > u64::MAX.into() {
                 return Err(GravityError::InvalidEventLogError(
                     "Nonce overflow, probably incorrect parsing".to_string(),
@@ -357,23 +445,28 @@ impl Erc20DeployedEvent {
             let index_start = 5 * 32;
             let index_end = index_start + 32;
             let denom_len = Uint256::from_bytes_be(&input.data[index_start..index_end]);
+
             // it's not probable that we have 4+ gigabytes of event data
             if denom_len > u32::MAX.into() {
                 return Err(GravityError::InvalidEventLogError(
                     "denom length overflow, probably incorrect parsing".to_string(),
                 ));
             }
+
             let denom_len: usize = denom_len.to_string().parse().unwrap();
             let index_start = 6 * 32;
             let index_end = index_start + denom_len;
             let denom = String::from_utf8(input.data[index_start..index_end].to_vec());
+
             trace!("Denom {:?}", denom);
+
             if denom.is_err() {
                 return Err(GravityError::InvalidEventLogError(format!(
                     "{:?} is not valid utf8, probably incorrect parsing",
                     denom
                 )));
             }
+
             let denom = denom.unwrap();
 
             // beyond this point we are parsing strings placed
@@ -386,44 +479,54 @@ impl Erc20DeployedEvent {
             let index_end = index_start + 32;
             let erc20_name_len = Uint256::from_bytes_be(&input.data[index_start..index_end]);
             // it's not probable that we have 4+ gigabytes of event data
+
             if erc20_name_len > u32::MAX.into() {
                 return Err(GravityError::InvalidEventLogError(
                     "ERC20 Name length overflow, probably incorrect parsing".to_string(),
                 ));
             }
+
             let erc20_name_len: usize = erc20_name_len.to_string().parse().unwrap();
             let index_start = index_end;
             let index_end = index_start + erc20_name_len;
             let erc20_name = String::from_utf8(input.data[index_start..index_end].to_vec());
+
             if erc20_name.is_err() {
                 return Err(GravityError::InvalidEventLogError(format!(
                     "{:?} is not valid utf8, probably incorrect parsing",
                     erc20_name
                 )));
             }
+
             trace!("ERC20 Name {:?}", erc20_name);
+
             let erc20_name = erc20_name.unwrap();
 
             let index_start = ((index_end + 31) / 32) * 32;
             let index_end = index_start + 32;
             let symbol_len = Uint256::from_bytes_be(&input.data[index_start..index_end]);
+
             // it's not probable that we have 4+ gigabytes of event data
             if symbol_len > u32::MAX.into() {
                 return Err(GravityError::InvalidEventLogError(
                     "Symbol length overflow, probably incorrect parsing".to_string(),
                 ));
             }
+
             let symbol_len: usize = symbol_len.to_string().parse().unwrap();
             let index_start = index_end;
             let index_end = index_start + symbol_len;
             let symbol = String::from_utf8(input.data[index_start..index_end].to_vec());
+
             trace!("Symbol {:?}", symbol);
+
             if symbol.is_err() {
                 return Err(GravityError::InvalidEventLogError(format!(
                     "{:?} is not valid utf8, probably incorrect parsing",
                     symbol
                 )));
             }
+
             let symbol = symbol.unwrap();
 
             let block_height = if let Some(bn) = input.block_number.clone() {
@@ -452,20 +555,24 @@ impl Erc20DeployedEvent {
     }
     pub fn from_logs(input: &[Log]) -> Result<Vec<Erc20DeployedEvent>, GravityError> {
         let mut res = Vec::new();
+
         for item in input {
             res.push(Erc20DeployedEvent::from_log(item)?);
         }
+
         Ok(res)
     }
     /// returns all values in the array with event nonces greater
     /// than the provided value
     pub fn filter_by_event_nonce(event_nonce: u64, input: &[Self]) -> Vec<Self> {
         let mut ret = Vec::new();
+
         for item in input {
             if item.event_nonce > event_nonce.into() {
                 ret.push(item.clone())
             }
         }
+
         ret
     }
 }
@@ -486,20 +593,24 @@ impl LogicCallExecutedEvent {
     }
     pub fn from_logs(input: &[Log]) -> Result<Vec<LogicCallExecutedEvent>, GravityError> {
         let mut res = Vec::new();
+
         for item in input {
             res.push(LogicCallExecutedEvent::from_log(item)?);
         }
+
         Ok(res)
     }
     /// returns all values in the array with event nonces greater
     /// than the provided value
     pub fn filter_by_event_nonce(event_nonce: u64, input: &[Self]) -> Vec<Self> {
         let mut ret = Vec::new();
+
         for item in input {
             if item.event_nonce > event_nonce.into() {
                 ret.push(item.clone())
             }
         }
+
         ret
     }
 }
@@ -508,9 +619,12 @@ impl LogicCallExecutedEvent {
 /// of ethereum events
 fn _debug_print_data(input: &[u8]) {
     let count = input.len() / 32;
+
     println!("data hex dump");
+
     for i in 0..count {
         println!("0x{}", bytes_to_hex_str(&input[(i * 32)..((i * 32) + 32)]))
     }
+
     println!("end dump");
 }
